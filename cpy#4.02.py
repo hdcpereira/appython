bd4aa6f1acceda5e398e91ee1ece5059a81f1bf1
hdcpereira <hugodavid@usp.br>
2019-04-25 15:01:06 -0300

    segundo desafio Curso em Vídeo

---
+++ cpy#4.02.py
@@ -0,0 +1,5 @@
+dia = input("qual o dia do seu nascimento?")
+mês = input("qual o mês do seu nascimento?")
+ano = input("qual o ano do seu nascimento?")
+
+print( "você nasceu em" ,dia,"/",mês,"/",ano,"?")
