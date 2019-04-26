8b8adf600baf6ac98aa275d75c07f1694430ca08
hdcpereira <hugodavid@usp.br>
2019-04-25 23:43:32 -0300

    resolução desafio 8

---
+++ desafio8.py
@@ -0,0 +1,4 @@
+medida = int(input("digite o valor da sua medida em metros "))
+medidacm = float(medida*10**2)
+medidamm = float(medida*10**3)
+print("se sua medida em metros é {} então ela em centímetros é {}cm e a em milimetros é {}mm".format(medida, medidacm, medidamm))
