97d3a9b895f695f029e5b9a0662d5cc1381f2f27
hdcpereira <hugodavid@usp.br>
2019-04-26 12:55:21 -0300

    modificado o input

--- desafio011.py
+++ desafio011.py
@@ -1,6 +1,6 @@
-h = int(input("qual a altura da sua parede? "))
-l = int(input("qual a largura da sua parede? "))
+h = float(input("qual a altura da sua parede? "))
+l = float(input("qual a largura da sua parede? "))
 a = h*l
 t = a*(1/2)
-print("se a altura da sua parede é {} e sua largura é {}, sua área é {} \n "
+print("se a altura da sua parede é {} e sua largura é {}, sua área é {}m2 \n "
       "e a quantidade de tinta que você vai precisar é {} litros.".format(h, l, a, t))
