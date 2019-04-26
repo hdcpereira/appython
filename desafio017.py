d55781fdfcebb50c1b79d2da102de7cbb2dbe5be
hdcpereira <hugodavid@usp.br>
2019-04-26 15:14:53 -0300

    resolução desafio 17
    kill

--- desafio017.py
+++
@@ -1,5 +0,0 @@
-import math
-catop =  float(input("digite o seu cateto oposto "))
-catad = float(input("digite o seu cateto adjacente "))
-hip = math.sqrt(pow(catop,2)+pow(catad,2))
-print("se seu cateto oposto é {} e seu cateto adjacente é {}, então sua hipotenusa é {}".format(catop,catad,hip))
