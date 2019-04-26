3b6439a9913d9bf489675c390f5f96575779e4cc
hdcpereira <hugodavid@usp.br>
2019-04-25 22:53:42 -0300

    deletados arquivos inúteis

--- cpy#4.02.py
+++
@@ -1,5 +0,0 @@
-dia = input("qual o dia do seu nascimento?")
-mês = input("qual o mês do seu nascimento?")
-ano = input("qual o ano do seu nascimento?")
-
-print( "você nasceu em" ,dia,"/",mês,"/",ano,"?")
--- cpy#4.03.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input('digite um número? '))
-n2 = float(input('digite outro número? '))
-n3 = int(n1 + n2)
-print('A sua soma  {}'.format(n3))
-print(type(n3))
