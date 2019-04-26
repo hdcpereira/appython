cf3a7a12b84e7d52d49cf4a829575c4bf0450373
hdcpereira <hugodavid@usp.br>
2019-04-25 23:36:43 -0300

    resolução do desafio 7

---
+++ desafio7.py
@@ -0,0 +1,4 @@
+nota1 = int(input("qual foi a sua nota na p1? "))
+nota2 = int(input("qual foi sua nota na p2? "))
+media = (nota1+nota2)/len([nota1,nota2])
+print("Então, se sua nota da p1 foi {} e a da p2 foi {}, sua média  {}".format(nota1,nota2,media))
