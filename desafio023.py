2701f42a12a098bd705d00625eda51e12f6d68c5
hdcpereira <hugodavid@usp.br>
2019-05-14 17:18:59 -0300

    resolução do desafio 23 atualizada

--- desafio023.py
+++ desafio023.py
@@ -1,8 +1,12 @@
-num = input("me diga um número de 0 a 9999 ")
+num = int(input("me diga um número de 0 a 9999 "))
+u = num // 1 % 10 
+d = num // 10 % 10 
+c = num // 100 % 10
+m = num // 1000 % 10  
 
 print("""
 milhar: {}
 dezena: {}
 centena: {}
 unidade: {}
-""" .format(num[0],num[1],num[2],num[3]))
+""" .format(u,d,c,m))
